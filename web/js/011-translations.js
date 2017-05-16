a2099269d867bc4df986506fbf8061a01ed61056
Paolo Benvenuto <paolobenve@gmail.com>
2017-05-16 14:43:04 +0200

    fix: missing translations file

---
+++ web/js/011-translations.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	
+	/* Globals: Translations 
+	 * 
+	 * Comment out the following statements if you don't need them
+	 * 
+	 * */
+	bydateTranslation = "immagini per data";                 // english: "images by date"
+	foldersTranslation = "cartelle";                         // english: "folders"
+	showMetadataTranslation = "Metadati";                    // english: "Metadata"
+	foldersViewTranslation = "cartelle";                     // english: "folders"
+	byDayTranslation = "per giorno";                         // english: "by day"
+	byMonthTranslation = "per mese";                         // english: "by month"
+	byYearTranslation = "per anno";                          // english: "by year"
+	donwloadOriginalTranslation = "scarica originale";       // english: "Download original"
+	fullscreenTranslation = "schermo intero";                // english: "Fullscreen"
+	loadingTranslation = "caricamento in corso...";          // english: "Loading..."
+	poweredByTranslation = "Realizzato con";                 // english: "Powered by"
+	errorTextTranslation = "Errore :-(";                     // english: "Forgot my camera"
+});
