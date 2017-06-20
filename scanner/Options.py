34f441fb4a6353da861a4ed0843003403b2b33f9
Paolo Benvenuto <paolobenve@gmail.com>
2017-06-20 19:52:15 +0200

    added Options.py

---
+++ scanner/Options.py
@@ -0,0 +1,65 @@
+#~ from CachePath import message
+
+class Options:
+	try:
+		Options
+	except NameError:
+		Options = {}
+	
+	DefaultOptions = {
+		'albumPath'                    : "", # absolute path
+		'cachePath'                    : "", # absolute path
+		'thumbSizes'                   : [ (1600, False), (1200, False), (800, False), (150, True) ],
+		'language'                     :"en", # overrides browser language
+		'js_zeroThumbSpacing'             : False,
+		'videoTranscodeBitrate'        : "4M",
+		'foldersString'                : "_folders",
+		'byDateString'                 : "_by_date",
+		'cacheFolderSeparator'         : "-",
+		'pageTitle'                    : "My photos",
+		'differentAlbumThumbnails'     : False,
+		'thumbnailsGenerationMode'     : "cascade", # permitted values: "cascade", "parallel", "mixed"
+		# 25 images ~ 5MB each, 4 thumbnail sizes:
+		# parallel ~ 60 seconds
+		# mixed    ~ 35 seconds
+		# cascade  ~ 30 seconds
+		#
+		# 25 images ~ 5MB each, 4 thumbnail sizes, the 4 already present:
+		# parallel ~ 48 seconds
+		# mixed    ~ 35 seconds
+		# cascade  ~ 0.2 seconds
+		'showMediaNamesBelowInAlbums'  : True,
+		'titleFontSize'                : "medium",	# other values: large, small, or a px/em size
+		'titleColor'                   : "white",
+		'titleImageNameColor'          : "green",
+		'jpegQuality'                  : 95,		# a number 1 -100
+		'backgroundColor'              : "#222222",	# ~ gray
+		'switchButtonBackgroundColor'  : "white",
+		'switchButtonColor'            : "black"
+	}
+	
+	OptionsForJs = [
+		'language',
+		'js_zeroThumbSpacing',
+		'foldersString',
+		'byDateString',
+		'cacheFolderSeparator',
+		'pageTitle',
+		'differentAlbumThumbnails',
+		'showMediaNamesBelowInAlbums',
+		'titleFontSize',
+		'titleColor',
+		'titleImageNameColor',
+		'backgroundColor',
+		'switchButtonBackgroundColor',
+		'switchButtonColor'
+	]
+
+	
+	def __init__(self):
+		for key in self.DefaultOptions :
+			try:
+				self.Options[key]
+			except KeyError:
+				self.Options[key] = self.DefaultOptions[key]
+
