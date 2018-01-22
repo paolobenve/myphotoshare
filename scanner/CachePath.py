177b202536683caa3638c45fe8ae4494b9dfe21c
Paolo Benvenuto <paolobenve@gmail.com>
2018-01-22 11:08:02 +0100

    new function for rationalizing code

--- scanner/CachePath.py
+++ scanner/CachePath.py
@@ -204,3 +204,39 @@ def checksum(path):
 			hasher.update(buf)
 			buf = afile.read(block_size)
 	return hasher.hexdigest()
+
+def square_thumbnail_sizes():
+	# collect all the square sizes needed
+
+	# album size: square thumbnail are generated anyway, because they are needed by the code that generates composite images for sharing albums
+	# the second element in the tuple il mobile_bigger
+	square_sizes = [(Options.config['album_thumb_size'], False)]
+	if Options.config['album_thumb_type'] == "square":
+		if Options.config['mobile_thumbnail_factor'] > 1:
+			square_sizes.append((Options.config['album_thumb_size'], True))
+	if Options.config['media_thumb_type'] == "square":
+		square_sizes.append((Options.config['media_thumb_size'], False))
+		if Options.config['mobile_thumbnail_factor'] > 1:
+			square_sizes.append((Options.config['media_thumb_size'], True))
+	# sort sizes descending
+	square_sizes = sorted(square_sizes, key=modified_size, reverse=True)
+	return square_sizes
+
+def modified_size(tuple):
+	(size, mobile_bigger) = tuple
+	if mobile_bigger:
+		return  int(round(size * Options.config['mobile_thumbnail_factor']))
+	else:
+		return size
+
+def thumbnail_types_and_sizes():
+	# collect all the square sizes needed
+	# album size: square thumbnail are generated anyway, because they are needed by the code that generates composite images for sharing albums
+	_thumbnail_types_and_sizes = {"square": square_thumbnail_sizes()}
+
+	if Options.config['album_thumb_type'] == "fit":
+		_thumbnail_types_and_sizes[Options.config['album_thumb_type']] = [(Options.config['album_thumb_size'], True), (Options.config['album_thumb_size'], False)]
+	if Options.config['media_thumb_type'] == "fixed_height":
+		_thumbnail_types_and_sizes[Options.config['media_thumb_type']] = [(Options.config['media_thumb_size'], True), (Options.config['media_thumb_size'], False)]
+
+	return _thumbnail_types_and_sizes
