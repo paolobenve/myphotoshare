c240c9a90125beb93a14cf85d7d81f80f000a49a
Pierre Métras <p.metras@videotron.ca>
2018-02-24 18:08:21 -0500

    Add new media if 'album.ini' already exists

--- bin/make_album_ini.sh
+++ bin/make_album_ini.sh
@@ -7,49 +7,52 @@
 # jpg,jpeg,JPG,mp4,avi,MP4,AVI
 
 
-if [ -z "$1" ]; then
+DIR=${1%/}
+
+if [ -z "$DIR" ]; then
 	echo "$0 FOLDER"
 	echo "Create template of 'album.ini' file in 'FOLDER' based on its media content."
+	echo "When the file 'album.ini' already exists in 'FOLDER', new media are added to the file."
 	exit 1
-fi
-
-
-DIR=${1%/}
-if [ ! -d "$DIR" ]; then
+elif [ ! -d "$DIR" ]; then
 	( >&2 echo "Argument must be a directory containing media." )
 	exit 1
 fi
 
-if [ -e "$DIR/album.ini" ]; then
-	( >&2 echo "'$DIR/album.ini' already exists. Nothing done.")
-	exit 2
+
+if [ ! -f "$DIR/album.ini" ]; then
+	touch "$DIR/album.ini"
 fi
 
-
-
-TITLE=${DIR##*/}
-echo "# User defined metadata for MyPhotoShare" >> "$DIR/album.ini"
-echo > "$DIR/album.ini"
-echo "[album]" >> "$DIR/album.ini"
-echo "#title = $TITLE" >> "$DIR/album.ini"
-echo "#description = " >> "$DIR/album.ini"
-echo "#tags = " >> "$DIR/album.ini"
-echo >> "$DIR/album.ini"
-echo >> "$DIR/album.ini"
+SECTION_EXISTS=$(grep -c "\[album\]" "$DIR/album.ini")
+if [ $SECTION_EXISTS -eq 0 ]; then
+	TITLE=${DIR##*/}
+	echo "# User defined metadata for MyPhotoShare" >> "$DIR/album.ini"
+	echo >> "$DIR/album.ini"
+	echo "[album]" >> "$DIR/album.ini"
+	echo "#title = $TITLE" >> "$DIR/album.ini"
+	echo "#description = " >> "$DIR/album.ini"
+	echo "#tags = " >> "$DIR/album.ini"
+	echo >> "$DIR/album.ini"
+	echo >> "$DIR/album.ini"
+fi
 
 SAVEIFS="$IFS"
 IFS=$(echo -en "\n\b")
 for media in $(ls "$DIR"/*.{jpg,jpeg,JPG,mp4,avi,MP4,AVI} 2> /dev/null); do
-	MEDIA=${media##*/}
-	MEDIA=${MEDIA%.*}
-	echo "[$MEDIA]" >> "$DIR/album.ini"
-	echo "#title = $MEDIA" >> "$DIR/album.ini"
-	echo "#description = " >> "$DIR/album.ini"
-	echo "#tags = " >> "$DIR/album.ini"
-	echo "#latitude = " >> "$DIR/album.ini"
-	echo "#longitude = " >> "$DIR/album.ini"
-	echo >> "$DIR/album.ini"
-	echo >> "$DIR/album.ini"
+	SECTION=${media##*/}
+	TITLE=${SECTION%.*}
+	SECTION_EXISTS=$(grep -c "\[$SECTION\]" "$DIR/album.ini")
+	if [ $SECTION_EXISTS -eq 0 ]; then
+		echo "[$SECTION]" >> "$DIR/album.ini"
+		echo "#title = $TITLE" >> "$DIR/album.ini"
+		echo "#description = " >> "$DIR/album.ini"
+		echo "#tags = " >> "$DIR/album.ini"
+		echo "#latitude = " >> "$DIR/album.ini"
+		echo "#longitude = " >> "$DIR/album.ini"
+		echo >> "$DIR/album.ini"
+		echo >> "$DIR/album.ini"
+	fi
 done
 IFS=$SAVEIFS
 
