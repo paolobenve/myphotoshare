2ac976eb8f2b313afb9514c453fa8eab927d63e5
Paolo Benvenuto <paolobenve@gmail.com>
2018-03-11 21:27:33 +0100

    bug with changed json_version type

--- scanner/PhotoAlbum.py
+++ scanner/PhotoAlbum.py
@@ -105,7 +105,7 @@ class Album(object):
 		self.album_ini = None
 		self._attributes = {}
 		self._attributes["metadata"] = {}
-		self.json_version = {}
+		self.json_version = ""
 
 		if (
 			Options.config['subdir_method'] in ("md5", "folder") and
@@ -295,7 +295,7 @@ class Album(object):
 		else:
 			path = dictionary["path"]
 		# Don't use cache if version has changed
-		if Options.json_version == 0 or "jsonVersion" not in dictionary or float(dictionary["jsonVersion"]) != Options.json_version:
+		if Options.json_version == 0 or "jsonVersion" not in dictionary or dictionary["jsonVersion"] != Options.json_version:
 			return None
 		album = Album(os.path.join(Options.config['album_path'], path))
 		album.cache_base = album_cache_base
